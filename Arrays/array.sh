d09b6f663668bef8eea7204ca9584fb85ccaf7e4
Kartik <kartik@hyperverge.co>
2020-09-05 19:37:44 +0530

    update array.sh

--- Arrays/array.sh
+++ Arrays/array.sh
@@ -3,7 +3,9 @@
 os=('Windows' 'Linux' 'Kali Linux' 'Max OS')
 
 echo "${os[@]}" # Print All the elements in the array 
-
 echo "${os[0]}" # Print 0th indice element
-
 echo "${!os[@]}" # Print all the indces in the array.
+echo "${#os[@]}" # Print Array Length
+
+string=kartiksinghrawat
+echo "${string[@]}"
