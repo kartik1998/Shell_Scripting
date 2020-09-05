9853b3dcbf0fd3ea14011abbe88d080a7faba609
Kartik <kartik@hyperverge.co>
2020-09-05 19:26:17 +0530

    Add Read Input

---
+++ ReadInput/read.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# echo "Enter name : "
+# read 
+# echo "Entered name : $REPLY"
+
+# ------------------------------------------------
+
+# echo "Enter name : "
+# read name
+# echo "Entered name : $name"
+
+# ------------------------------------------------
+
+# echo "Enter names : "
+# read name1 name2 name3
+# echo "Entered names : $name1 , $name2 , $name3"
+
+# ------------------------------------------------
+
+read -p "Enter User name : " user_name
+read -sp "Enter User password : " user_pwd
+echo 
+echo "User Details : $user_name , $user_pwd"
---
+++ ReadInput/readArray.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "Enter names : "
+read -a names
+echo "First two names : ${names[0]} ${names[1]}"
