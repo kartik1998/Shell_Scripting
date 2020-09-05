518f83c0267dede5c0a9418cd7fc73e43929f45c
Kartik <kartik@hyperverge.co>
2020-09-05 19:28:45 +0530

    Add arrays

---
+++ Arrays/array.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+os=('Windows' 'Linux' 'Kali Linux' 'Max OS')
+
+echo "${os[@]}" # Print All the elements in the array 
+
+echo "${os[0]}" # Print 0th indice element
+
+echo "${!os[@]}" # Print all the indces in the array.
