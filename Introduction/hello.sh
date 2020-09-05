a81bdcb8ccb743a39d5e62040e3155a016fec9b0
Kartik <kartik@hyperverge.co>
2020-09-05 19:25:57 +0530

    Add introduction

---
+++ Introduction/hello.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# Change permission : chmod +x hello.sh 
+
+echo "Hello World"
+
+echo OS shell name is : $BASH
+echo OS bash version is : $BASH_VERSION
+echo Home Directory : $HOME
+echo Present Working Directory : $PWD
+
+name=Kartik
+val=10
+echo My name is $name
+echo Variable : $val
